c76ed67e0caa9f8724c7aa12c45f0c977a0f1b02
Podter <48936015+Podter@users.noreply.github.com>
2023-01-08 13:35:26 +0700

    native-base

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Text, View } from 'react-native';
+import { NativeBaseProvider, Text, Box } from 'native-base';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello, world!</Text>
-    </View>
+    <NativeBaseProvider>
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+        <Text>Open up App.js to start working on your app!</Text>
+      </Box>
+    </NativeBaseProvider>
   );
 }
