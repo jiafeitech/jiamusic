ecc98bbf1befd687d8607753a58a415c920cc9fd
Podter <48936015+Podter@users.noreply.github.com>
2023-01-08 15:08:52 +0700

    theme support

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 // Imports
-import { NavigationContainer } from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Provider as PaperProvider } from 'react-native-paper';
+import {
+  Provider as PaperProvider,
+  MD3DarkTheme,
+  MD3LightTheme,
+  adaptNavigationTheme,
+} from 'react-native-paper';
+import merge from 'deepmerge';
+import { useColorScheme } from 'react-native';
+import { useEffect } from 'react';
 
 // Types
 import StackParamList from './types/StackParamList';
@@ -18,17 +30,34 @@ import Navbar from './components/Navbar';
 import Home from './screens/Home';
 import Player from './screens/Player';
 
+// Theme
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: NavigationDefaultTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
+
+const CombinedDefaultTheme = merge(MD3LightTheme, LightTheme);
+const CombinedDarkTheme = merge(MD3DarkTheme, DarkTheme);
+
 // Native Stack
 const Stack = createNativeStackNavigator<StackParamList>();
 
 // App
 export default function App() {
+  const colorScheme = useColorScheme();
+
   return (
     <PocketBaseProvider>
       <SongListProvider>
         <CurrentSongProvider>
-          <PaperProvider>
-            <NavigationContainer>
+          <PaperProvider
+            theme={
+              colorScheme == 'dark' ? CombinedDarkTheme : CombinedDefaultTheme
+            }>
+            <NavigationContainer
+              theme={
+                colorScheme == 'dark' ? CombinedDarkTheme : CombinedDefaultTheme
+              }>
               <Stack.Navigator
                 initialRouteName="Home"
                 screenOptions={{
